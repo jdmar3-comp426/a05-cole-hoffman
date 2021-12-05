7b3aa0a9a094c6ec11bb4500e0f70e6042a0168a
cole-hoffman <cwh@live.unc.edu>
2021-12-04 20:36:44 -0500

    done

---
+++ forms.js
@@ -0,0 +1,42 @@
+window.addEventListener("load", function(){
+    
+    //start add user
+    function addAccount() {
+        const XHR = new XMLHttpRequest()
+         // Bind the FormData object and the form element
+        const FD = new URLSearchParams(new FormData( form ));
+
+
+        // Define what happens on successful data submission
+        XHR.addEventListener( "load", function(event) {
+            alert( 'Logged in! :)' );
+            //localStorage.setItem( "currentUserId", JSON.parse( XHR.response ).getElementById );
+            //alert( "currentUserId: " + localStorage.getItem( "currentUserId" ) );
+        } );
+
+        // Define what happens in case of error
+        XHR.addEventListener( "error", function( event ) {
+            alert( 'Oops! Something went wrong.' );
+        } );
+
+        // Set up our request
+        XHR.open( "POST", "http://localhost:5500/app/new", true );
+
+        // The data sent is what the user provided in the form
+        XHR.send( FD );
+    }
+
+    // Access the form element...
+    const form = document.getElementById( "forms" );
+
+    // Submit account to database after clicking submit
+    form.addEventListener( "submit", function ( event ) {
+        event.preventDefault();
+
+        addAccount();
+
+    } )
+    //end add user
+
+    //
+})
